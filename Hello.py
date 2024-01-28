58c8db9e46f94e0dba3870750061c0d3305c98c3
RabeccaNdhlovu <157544919+RabeccaNdhlovu@users.noreply.github.com>
2024-01-28 17:54:54 +0000

    change made to timelog

--- Hello.py
+++ Hello.py
@@ -1,51 +1,25 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from datetime import datetime
 
-LOGGER = get_logger(__name__)
+def main():
+    st.title("Time Log System")
 
+    Start = st.button("Start Time") 
+    End = st.button("End Time")
+    
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    if Start:
+        st.session_state.start_timestamp = datetime.now()
+        st.success(f"Start time set to {st.session_state.start_timestamp}")
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+    if End:
+        if "start_timestamp" in st.session_state:
+            end_timestamp = datetime.now()
+            time_difference = end_timestamp - st.session_state.start_timestamp
+            st.success(f"End time set to {end_timestamp}")
+            st.info(f"Time difference: {time_difference}")
+        else:
+            st.warning("Please set the start time first.")
 
 if __name__ == "__main__":
-    run()
+    main()
